3fab380d1cf3569bf149022ca967ba2d27ff7e11
DaveKnud <daveknud@outlook.com>
2023-05-05 01:57:20 +0200

    creato il previous button

--- main.js
+++ main.js
@@ -10,10 +10,45 @@ let ActiveImg = 0;
 const next = document.getElementById("next");
 console.log(next);
 
+//select tasto previous
+const previous = document.getElementById("previous");
+console.log(previous);
+
+
 //creo active al tasto next
 next.addEventListener('click',
     function(){
-        img[ActiveImg].classList.remove("active");
-        img[ActiveImg].classList.add("inactive")
-        console.log("ciao") }
+
+        if(ActiveImg < (img.length - 1)) {
+
+            img[ActiveImg].classList.remove("active");
+            img[ActiveImg].classList.add("inactive");
+            console.log("ciao");
+    
+            //Cambia immagine passare a imm successiva
+            ActiveImg = ActiveImg + 1;
+    
+            //immagini successive cosa fa
+            img[ActiveImg].classList.remove("inactive");
+            img[ActiveImg].classList.add("active");
+        }
+    }
+)
+
+previous.addEventListener('click',
+    function(){
+        if(ActiveImg > (img.length = 0)) {
+
+            img[ActiveImg].classList.remove("active");
+            img[ActiveImg].classList.add("inactive");
+            console.log("ciao");
+
+            //Cambia immagine passare a imm successiva
+            ActiveImg = ActiveImg - 1;
+
+            //immagini successive cosa fa
+            img[ActiveImg].classList.remove("inactive");
+            img[ActiveImg].classList.add("active");
+        }
+    }
 )
