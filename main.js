956d6a91bcb75a15d3820f7cdf4e7ce0b373faa2
DaveKnud <daveknud@outlook.com>
2023-05-04 17:36:45 +0200

    non va il bottone

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ const img = document.getElementsByClassName("img-container");
 console.log(img);
 
 
+//immagine attiva al momento
+let ActiveImg = 0;
+
+//Select tasto Next
+const next = document.getElementsByClassName("next");
+console.log(next);
+
+//creo active al tasto next
+next.addEventListener('click',
+    function(){
+        img[ActiveImg].classList.remove("active");
+        console.log("ciao")  }
+)
